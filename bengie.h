f5144b928d3b8fca6a8f19ce2986ec2565fdd9e7
--get <angiedelgado08@gmail.com>
2018-11-12 04:07:56 +0000

    bengie.h

--- bengie.h
+++ bengie.h
@@ -1,4 +1,10 @@
 #ifndef _BENGIE_H
 #define _BENGIE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #endif _BENGIE_H

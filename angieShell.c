622883474aa936d583d8e3135c86c373ae0e3d84
VieetBubbles <537@holbertonschool.com>
2018-11-18 21:41:45 +0000

    Added the signal function to handle Ctrl + D and C.

--- angieShell.c
+++ angieShell.c
@@ -1,5 +1,12 @@
 #include "bengie.h"
 
+void sigintHandler(int bob)
+{
+        (void) bob;
+
+        write(STDOUT_FILENO, "\nBengie$ ", 9);
+}
+
 int main()
 {
         pid_t child_pid = 0;
@@ -10,38 +17,60 @@ int main()
         int i = 1;
         ssize_t chars;
 
-
         while (1)
         {
-                write(STDIN_FILENO, "$ ", 2);
+                if (isatty(0))
+                {
+                        write(STDIN_FILENO, "Bengie$ ", 8);
+                }
+                signal(SIGINT, sigintHandler);
+
+                /*write(STDIN_FILENO, "$ ", 2);*/
                 child_pid = fork();
                 if (child_pid == -1)
-			perror("fork() Error")
                         return (-1);
                 if (child_pid == 0)
                 {
                         chars = (getline(&buffer, &buffsize, stdin));
+
                         if (chars == - 1)
-                        {
-				perror ("getline error")
-				return (-1);
+			{
+                                if (isatty(0))
+                                        write(STDIN_FILENO, "\n", 1);
+                                free(buffer);
+                                return (-1);
                         }
+                        if (chars == 0)
+                        {
+                                if (isatty(0))
+                                {
+                                        write(STDIN_FILENO, "\n", 1);
+                                        continue;
+                                }
+                        }
+
                         if ((buffer)[chars - 1] == '\n')
                         {
                                 (buffer)[chars - 1] = '\0';
-                        }
+			}
                         token[0] = strtok(buffer, " ");
                         while (token[i] != NULL)
                         {
                                 token[i] = strtok(NULL, " ");
                                 i++;
                         }
-			if (execve(token[0], token, NULL) == -1)
+                        if (execve(token[0], token, NULL) == -1)
                         {
                                 perror("Error on Execve");
-				return (-1);
-			}
-                }
+                        }
+                        return (-1);
+                        /*if (strcmp(argv[0], "env") == 0)
+                        {
+                                print_env();
+                                free(argv);
+                                break;
+                                }*/
+		}
                 if (child_pid > 0)
                 {
                         wait(&status);

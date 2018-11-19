c63aad79706a39a1858d07c2f04175ff2a667884
VieetBubbles <537@holbertonschool.com>
2018-11-19 19:23:42 +0000

    Fixed indent errors.

--- builtin_functions.c
+++ builtin_functions.c
@@ -2,22 +2,22 @@
 
 int (*_get_built_ins(char *s))(char **agrs)
 {
-        op_t ops[] = {
-                {"env", print_env},
-                {"exit", builtin_exit},
-                {NULL, NULL}
-        };
+	op_t ops[] = {
+		{"env", print_env},
+		{"exit", builtin_exit},
+		{NULL, NULL}
+	};
 
-        int i;
+	int i;
 
-        i = 0;
-        while (ops[i].op != NULL)
-        {
-                if (strcmp(ops[i].op, *s, strlen(ops[i].op)) == 0)
-                {
-                        break;
-                }
-                i++;
-        }
-        return (ops[i].f);
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, *s, strlen(ops[i].op)) == 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return (ops[i].f);
 }
